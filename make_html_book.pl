3cdd0b050f63dbf7c8776e74a6a7c7b867a5f323
Ed J <mohawk2@users.noreply.github.com>
2024-12-27 00:07:55 +0000

    Piddle -> NDArray

--- make_html_book.pl
+++ make_html_book.pl
@@ -24,7 +24,7 @@ Matthew Kenworthy <matthew.kenworthy@gmail.com>, 2012 Jan 25
 # USER SELECTABLE OPTIONS
 
 my $book_dir        = "PDL/Book/"; # relative path to the PDL Book files
-my $book_chapters   = "../Book FirstSteps Piddle Creating NiceSlice Functions Threading PGPLOT PLplot graphics_3d Transform Complex Pthreads PP Genesis Credits"; # an ordered list of POD files to process
+my $book_chapters   = "../Book FirstSteps NDArray Creating NiceSlice Functions Threading PGPLOT PLplot graphics_3d Transform Complex Pthreads PP Genesis Credits"; # an ordered list of POD files to process
 
 my $book_pdf        = "../pdl-www/content/book/index.html"; # output name of book
 
